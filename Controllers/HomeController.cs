d5185ae70e568c2b28c826f7960e737c0bffdbd9
Sathiyaraman <Sathiyaraman2003@gmail.com>
2021-02-18 20:39:30 +0530

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
